3b5be433baaada6b3f326ee53cbc8bc7ce108e2a
Yasar Arafath <55400911+Yasar1895@users.noreply.github.com>
2025-05-22 12:49:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,284 @@
+// console.log("Welcoem to the page..");
+// document.writeln("<h1>Hello welcome</h1>");
+
+// var a = "surya"
+// var a= "yasar"
+// console.log(a);
+// let b = 12;
+// b=13
+// console.log(b);
+// const y = "yaasar "
+// y =99
+// console.log(y);
+
+// alert('welcome to the page...')
+// prompt('enter your name')
+// confirm(' r u exit the page..')
+// print('')
+// // close()
+// open('https://flexboxfroggy.com/')
+
+
+// let a = 10
+
+// console.log(a.toString());
+// console.log(a);
+
+
+// let b = "100"
+
+// console.log(parseInt(b));
+
+
+// let Name = "  suryaraj  "
+
+// console.log(Name);
+// console.log(Name[0]);
+// console.log(Name.concat(Name));
+// console.log(Name.indexOf('s'));
+// console.log(Name.lastIndexOf('r'));
+// console.log(Name.length);
+// console.log(Name.repeat(3));
+// console.log(Name.toUpperCase());
+// console.log(Name.trim());
+
+// Name[7]="ss"
+
+// console.log(Name);
+
+// console.log(Name);
+// console.log(Math.E);
+// console.log(Math.PI);
+// console.log(Math.abs(-9.8));
+// console.log(Math.ceil(1.1));
+// console.log(Math.floor(12.22222222));
+// console.log(Math.max(90,80));
+// console.log(Math.min(70,40));
+// console.log(Math.pow(2,5));
+// console.log(Math.random());
+// console.log(Math.round(4.5));
+// console.log(Math.PI);
+
+
+// let a = Math.PI
+
+// let b = a.toString()
+
+// let c =b.slice(0,4)
+
+// let d = parseFloat(c)
+// console.log(d);
+
+// let e = a.toFixed(2)
+
+// console.log(e);
+
+
+
+
+
+// Arithmetic +,-,%,*,/  
+
+
+//  assonment = +=,-=,/=,*= 
+
+// comparisaion  < > <= >= !=  ==  ===
+
+// logical  AND &&  OR ||  NOT !
+
+// increment ++ --
+
+//  & | ~ 
+
+
+//  value ? true : false 
+
+
+// if(10<4){
+//     console.log("welcome...");
+    
+// }
+
+// else if((10<40)&&(40<5)){
+//     console.log("2nd value is correct..");
+    
+// }
+
+// else if((10<4)||(50<6)){
+//     console.log("3rd correct...");
+    
+// }
+// else{
+//     console.log("error...");
+    
+// }
+
+// if(40<50){
+//     if(50<60){
+//         if(60<80){
+//             console.log("runing...");
+            
+//         }
+//     }
+// }
+
+
+// let a =1
+
+//  switch (a){
+//     case 0:
+//         console.log("Zero...");
+//         break;
+
+//     case 1:
+//         console.log("One..");
+         
+
+//     case 2:
+//         console.log("Two..");
+//         break;
+
+//     case 3:
+//         console.log("three..");
+//         break ;
+
+//     default:
+//         console.log("error..");
+//         break;      
+        
+        
+        
+//  }
+
+
+
+// let Arr = [1,2.3,'s',"Surya",true]
+
+// console.log(Arr);
+// console.log(Arr[0]);
+
+// console.log(Arr.concat(Arr));
+
+// Arr[5]="Yasir"
+// Arr.push('welcome1..')
+// Arr.push('welcome2..')
+// Arr.push('welcome3..')
+// Arr.pop()
+
+// Arr.unshift('Rishi1')
+// Arr.unshift('Rishi2')
+// Arr.unshift('Rishi3')
+// Arr.shift()
+
+// Arr.splice(2,1,"KIA")
+
+
+// console.log(Arr);
+
+
+// let MuAr = [
+//     [1,2,3,4],[5,6,7,['a','b','b','c',[1,2,3,4,[6,7,8,9]]]]
+// ]
+
+
+// const ARR = [1,2,3]
+
+// ARR.push(80);
+
+// console.log(ARR);
+
+// let obj = {
+//      Name :"SURYRAJ",
+//      AGE:12,
+//      CITY :"ERODE",
+//      Brother:{
+//         NAme :"KARTHICK"
+//      }
+     
+// }
+
+// console.log(obj.Brother.NAme);
+
+
+
+
+
+let ArrObj =[
+    {NAME:"SURYA1"},
+    {NAME:"SURYA2"},
+    {NAME:"SURYA3"},
+    {NAME:"SURYA4"},
+    {NAME:"SURYA5"},
+    {NAME:"SURYA6"},
+]
+
+// console.log(ArrObj[4].NAME);
+
+
+for(let i=0;i<10;i++){
+    console.log(i);  
+}
+
+let a =10
+// while(a<20){
+//     a++
+//     console.log(a);
+    
+// }
+
+// do{
+//         console.log(a);
+//         a++
+//         if(a==15){
+//             break
+//         }
+        
+// }while(a<20)
+
+
+
+// for(let y in ArrObj){
+//     console.log(y);
+    
+// }
+
+// ArrObj.forEach((i)=>{
+//     console.log(i);
+    
+// })
+
+// ArrObj.map((o)=>{
+//     console.log(o);
+    
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
